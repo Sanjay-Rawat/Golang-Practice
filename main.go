927c8d2cfac12f8b541b01e257778c579a305441
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2022-04-22 08:46:34 +0530

    basic variable declairation

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a int = 12
-	fmt.Print(a)
+	VariableDeclairation()
+
 }
---
+++ variable.go
@@ -0,0 +1,10 @@
+package main
+
+func VariableDeclairation() {
+	var a int = 100
+	println(a)
+	b := 200
+	println(b)
+	flag := true
+	println(flag)
+}
